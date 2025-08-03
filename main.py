694ae33f800b37a172e6455d1fee812abb4798cb
Filipe-leite <filipedasilva94.1@gmail.com>
2025-08-03 02:26:13 +0100

    created a schema for the routes with the business rules for the test specifications

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
+# Reset state before starting tests
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/reset")
+async def reset_state():
+    return
+
+# Get balance for non-existing account
+
+@app.get("/balance")
+async def get_balance():
+    return
+
+# Create account with initial balance
+# or
+# Deposit into existing account
+# and
+# Withdraw from existing account
+# or
+# Withdraw from non-existing account
+# and
+# Transfer from existing account
+# or
+# Transfer from non-existing account
+
+@app.post("/event")
+async def create_account_or_deposit_account():
+    return
+
+# Get balance for existing account
+
+@app.post("/balance")
+async def create_account_or_deposit_account():
+    return
